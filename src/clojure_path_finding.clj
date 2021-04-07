4742a403b6b8fc56bff32a178283ed415686f29b
gordon_from_blumberg <gordon.from.blumberg@gmail.com>
2021-04-07 20:52:57 +0400

    Build graph

--- src/clojure_path_finding.clj
+++ src/clojure_path_finding.clj
@@ -2,12 +2,22 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(println "Hello world" (reduce + (range 1 11)))
+(println (macroexpand '(->> (io/reader file)
+                            (line-seq)
+                            (map #(clojure.string/split % #" "))
+                            (map #([(set (subvec % 0 2))
+                                    (get % 2)]))
+                            )
+                      ))
 
-(println (macroexpand '(->> (io/reader "test.txt")
-                           (line-seq)
-                           (map #(str/split % #" "))
-                           )))
-(println (->> (io/reader "test.txt")
-              (line-seq)
-              (map #(clojure.string/split % #" "))))
+(defn read-graph [file] (->> (io/reader file)
+                             (line-seq)
+                             (map #(clojure.string/split % #" "))
+                             (map #([(set (subvec % 0 2))
+                                     (get % 2)]))
+                             )
+  )
+
+(println (read-graph "test.txt"))
+
+         ;(let [graph ()])
