baf0d762ec61d08c68ed2d4638426dac349420ba
gordon_from_blumberg <gordon.from.blumberg@gmail.com>
2021-04-29 21:18:23 +0400

    Build graph

--- src/clojure_path_finding.clj
+++ src/clojure_path_finding.clj
@@ -2,22 +2,41 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(println (macroexpand '(->> (io/reader file)
-                            (line-seq)
-                            (map #(clojure.string/split % #" "))
-                            (map #([(set (subvec % 0 2))
-                                    (get % 2)]))
-                            )
-                      ))
+;(println (macroexpand '(->> (io/reader file)
+;                            (line-seq)
+;                            (map #(clojure.string/split % #" "))
+;                            (map #([(set (subvec % 0 2))
+;                                    (get % 2)]))
+;                            )
+;                      ))
 
-(defn read-graph [file] (->> (io/reader file)
-                             (line-seq)
-                             (map #(clojure.string/split % #" "))
-                             (map #([(set (subvec % 0 2))
-                                     (get % 2)]))
-                             )
-  )
+;(defn read-graph [file] (let [lines (line-seq (io/reader file))] {
+;                                                                  [:number :target] (str/split (first lines) " ")
+;                                                                  :points
+;                                                                  }
+;                          )
+;)
+
+(defn read-graph [file]
+  (let [lines (->> (io/reader file)
+                   (line-seq))
+        size-tar (-> (first lines)
+                    (str/split #" "))] {
+                                        :size   (get size-tar 0)
+                                        :target (get size-tar 1)
+                                        :points (->> (rest lines)
+                                                     (map #(str/split % #" "))
+                                                     (map #(vector (set (subvec % 0 2))
+                                                                   (get % 2))))
+                                        }
+
+  ))
 
 (println (read-graph "test.txt"))
+
+(println (map #(vector (set (subvec % 0 2))
+                       (get % 2)) '([1 2 2] [1 3 1])))
+
+(let [gr (read-graph "test.txt")] (println (:size gr) (:target gr)))
 
          ;(let [graph ()])
