4cd1be5be39c2b5f885bf9eec6d0ba6a4842cf7d
gordon_from_blumberg <gordon.from.blumberg@gmail.com>
2021-04-30 21:26:19 +0400

    Function siblings

--- src/clojure_path_finding.clj
+++ src/clojure_path_finding.clj
@@ -2,41 +2,40 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-;(println (macroexpand '(->> (io/reader file)
-;                            (line-seq)
-;                            (map #(clojure.string/split % #" "))
-;                            (map #([(set (subvec % 0 2))
-;                                    (get % 2)]))
-;                            )
-;                      ))
+(defn get-int [coll index]
+  (Integer/parseInt (get coll index)))
 
-;(defn read-graph [file] (let [lines (line-seq (io/reader file))] {
-;                                                                  [:number :target] (str/split (first lines) " ")
-;                                                                  :points
-;                                                                  }
-;                          )
-;)
+(defn int-set [coll]
+  (->> (set coll)
+       (map #(Integer/parseInt %))
+       set))
 
 (defn read-graph [file]
   (let [lines (->> (io/reader file)
-                   (line-seq))
+                   line-seq)
         size-tar (-> (first lines)
                     (str/split #" "))] {
-                                        :size   (get size-tar 0)
-                                        :target (get size-tar 1)
+                                        :size   (get-int size-tar 0)
+                                        :target (get-int size-tar 1)
                                         :points (->> (rest lines)
                                                      (map #(str/split % #" "))
-                                                     (map #(vector (set (subvec % 0 2))
-                                                                   (get % 2))))
+                                                     (map #(vector (int-set (subvec % 0 2))
+                                                                   (get-int % 2))))
                                         }
 
   ))
 
-(println (read-graph "test.txt"))
+(defn siblings [graph point]
+  (println "point =" point)
+  (->> (:points graph)
+       (map #(get % 0))
+       (filter #(contains? % point))
+       (map #(-> (disj % point)
+                 first))
+       ))
 
-(println (map #(vector (set (subvec % 0 2))
-                       (get % 2)) '([1 2 2] [1 3 1])))
+(def graph (read-graph "test.txt"))
 
-(let [gr (read-graph "test.txt")] (println (:size gr) (:target gr)))
+(println graph)
 
-         ;(let [graph ()])
+(println (last (siblings graph 12) ))
